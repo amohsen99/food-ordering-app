6c1f1e9314e7e6a90e5d5c70b38783372d3aa957
mohsen <amohsenmohamed571@gmail.com>
2025-06-23 14:39:13 +0300

    [ADD]: add logic to apear and disable menu in sm layout

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -4,6 +4,8 @@ import { Pages, Routes } from "@/constants/enums";
 import Link from "../link";
 import { Button, buttonVariants } from "../ui/button";
 import { useState } from "react";
+import { Menu, XIcon } from "lucide-react";
+
 
 function Navbar() {
     const [openMenu, setOpenMenu] = useState(false);
@@ -32,8 +34,27 @@ function Navbar() {
   return (
     <nav className="flex justify-end flex-1">
       {/* <div> */}
-        <ul  className={`fixed lg:static  top-0 px-10 py-20 lg:p-0 bg-background lg:bg-transparent transition-all duration-200 h-full lg:h-auto flex-col lg:flex-row w-full lg:w-auto flex lg:items-center gap-10`}
+       <Button
+        variant="secondary"
+        size="sm"
+        className="lg:hidden"
+        onClick={() => setOpenMenu(true)}
       >
+        <Menu className="!w-6 !h-6" />
+      </Button>
+        <ul  className={`fixed lg:static ${
+          openMenu ? "left-0 z-50" : "-left-full"
+        } top-0 px-10 py-20 lg:p-0 bg-background lg:bg-transparent transition-all duration-200 h-full lg:h-auto flex-col lg:flex-row w-full lg:w-auto flex items-start lg:items-center gap-10`}
+   
+      >
+        <Button
+          variant="secondary"
+          size="sm"
+          className="absolute top-10 right-10 lg:hidden"
+          onClick={() => setOpenMenu(false)}
+        >
+          <XIcon className="!w-6 !h-6" />
+        </Button> 
           {links.map((link) => (
           <li key={link.id}>
             <Link
@@ -48,6 +69,7 @@ function Navbar() {
           </li>
         ))}
         </ul>
+        
         
       {/* </div> */}
     </nav>
