852325326dce786796778d0cf8fc5300e884f49c
abdelMohsen571 <amohsen.mohamed571@gmail.com>
2025-06-24 08:45:27 +0300

    [FIX]: fix google font issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Roboto } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 
@@ -10,10 +10,7 @@ const roboto = Roboto({
   preload:true 
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
