9fb52f6ec7760d38ba0b36fa1b9b1d07f2164fa6
abdelMohsen571 <amohsen.mohamed571@gmail.com>
2025-06-24 15:34:50 +0300

    [ADD]: install and init prisma

--- src/components/menu/add-to-cart-button.tsx
+++ src/components/menu/add-to-cart-button.tsx
@@ -5,12 +5,13 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { formatCurrency } from '@/lib/formatters';
-import { Checkbox } from '@radix-ui/react-checkbox';
+import { Checkbox } from '@/components/ui/checkbox';
 import Image from 'next/image';
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
@@ -23,6 +24,11 @@ function AddToCartButton({ item }: { item: any }) {
       { id: 'small', name: 'Small', price: 2 },
       { id: 'medium', name: 'Medium', price: 4 },
       { id: 'large', name: 'Large', price:6 },
+    ];
+    const extras = [
+      { id: 'extra-cheese', name: 'Extra Cheese', price: 1.5 },
+      { id: 'extra-sauce', name: 'Extra Sauce', price: 1.0 },
+      { id: 'extra-toppings', name: 'Extra Toppings', price: 2.0 },
     ];
   return (
     <Dialog>
@@ -41,6 +47,17 @@ function AddToCartButton({ item }: { item: any }) {
             <PickSize item={item} sizes={sizes} />
           </div>
         </div>
+         <div className='space-y-10'>
+          <div className='space-y-4 text-center'>
+            <Label htmlFor='pick-size'>Add Extras</Label>
+            <Extras extras={extras} />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button className='w-full bg-orange-500 hover:bg-orange-600 text-white'>
+            Add to Cart {formatCurrency(item.basePrice)}
+          </Button>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
@@ -68,41 +85,41 @@ function PickSize({
     </RadioGroup>
   );
 }
-// function Extras({
-//   extras,
-//   selectedExtras,
-//   setSelectedExtras,
-// }: {
-//   extras: Extra[];
-//   // selectedExtras: Extra[];
-//   // setSelectedExtras: React.Dispatch<React.SetStateAction<Extra[]>>;
-// }) {
-//   // const handleExtra = (extra: Extra) => {
-//   //   if (selectedExtras.find((e) => e.id === extra.id)) {
-//   //     const filteredSelectedExtras = selectedExtras.filter(
-//   //       (item) => item.id !== extra.id
-//   //     );
-//   //     setSelectedExtras(filteredSelectedExtras);
-//   //   } else {
-//   //     setSelectedExtras((prev) => [...prev, extra]);
-//   //   }
-//   // };
-//   return extras.map((extra) => (
-//     <div
-//       key={extra.id}
-//       className='flex items-center space-x-2 border border-gray-100 rounded-md p-4'
-//     >
-//       <Checkbox
-//         id={extra.id}
-//         onClick={() => handleExtra(extra)}
-//         checked={Boolean(selectedExtras.find((e) => e.id === extra.id))}
-//       />
-//       <Label
-//         htmlFor={extra.id}
-//         className='text-sm text-accent font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-//       >
-//         {extra.name} {formatCurrency(extra.price)}
-//       </Label>
-//     </div>
-//   ));
-// }
+function Extras({
+  extras,
+  // selectedExtras,
+  // setSelectedExtras,
+}: {
+  extras: any;
+  // selectedExtras: Extra[];
+  // setSelectedExtras: React.Dispatch<React.SetStateAction<Extra[]>>;
+}) {
+  // const handleExtra = (extra: Extra) => {
+  //   if (selectedExtras.find((e) => e.id === extra.id)) {
+  //     const filteredSelectedExtras = selectedExtras.filter(
+  //       (item) => item.id !== extra.id
+  //     );
+  //     setSelectedExtras(filteredSelectedExtras);
+  //   } else {
+  //     setSelectedExtras((prev) => [...prev, extra]);
+  //   }
+  // };
+  return extras.map((extra:any) => (
+    <div
+      key={extra.id}
+      className='flex items-center space-x-2 border border-gray-100 rounded-md p-4'
+    >
+      <Checkbox
+        id={extra.id}
+        // onClick={() => handleExtra(extra)}
+        // checked={Boolean(selectedExtras.find((e) => e.id === extra.id))}
+      />
+      <Label
+        htmlFor={extra.id}
+        className='text-sm text-accent font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+      >
+        {extra.name} {formatCurrency(extra.price)}
+      </Label>
+    </div>
+  ));
+}
