0b486706a8ca8fd00113475e3da14afceee74282
mohsen <amohsenmohamed571@gmail.com>
2025-07-09 12:52:19 +0300

    [UPDATE]:update redirecting the pages based on role
    if role admin:it will redirect to admin page
    if role user :it will redirect to profile page

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Pages, Routes } from "@/constants/enums";
+import { Locale } from "@/i18n.config";
+import getTrans from "@/lib/translation";
+import { authOptions } from "@/server/auth";
+import { UserRole } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-const AdminPage = () => {
+async function AdminPage({ params }: { params: Promise<{ locale: Locale }> }) {
+  const { locale } = await params;
+  const translations = await getTrans(locale);
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect(`/${locale}/${Routes.AUTH}/${Pages.LOGIN}`);
+  }
+
+  if (session && session.user.role !== UserRole.ADMIN) {
+    redirect(`/${locale}/${Routes.PROFILE}`);
+  }
   return (
-    <div>AdminPage</div>
-  )
+    <main>
+      <section className="section-gap">
+        <div className="container">
+            admin page
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default AdminPage
+export default AdminPage;
--- src/app/[locale]/profile/page.tsx
+++ src/app/[locale]/profile/page.tsx
@@ -1,10 +1,29 @@
+import { Pages, Routes } from "@/constants/enums";
+import { Locale } from "@/i18n.config";
+import getTrans from "@/lib/translation";
+import { authOptions } from "@/server/auth";
+import { UserRole } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
- function ProfilePage({}) {
+async function ProfilePage({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const session = await getServerSession(authOptions);
+  const { locale } = await params;
+  const translations = await getTrans(locale);
 
-
+  if (!session) {
+    redirect(`/${locale}/${Routes.AUTH}/${Pages.LOGIN}`);
+  }
+  if (session && session.user.role === UserRole.ADMIN) {
+    redirect(`/${locale}/${Routes.ADMIN}`);
+  }
   return (
     <main>
-      test
+   <div>profile page</div>
     </main>
   );
 }
