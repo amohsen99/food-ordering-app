f4d604f2b64c05a8134f182b51eea5480a00a503
mohsen <amohsenmohamed571@gmail.com>
2025-07-10 15:24:54 +0300

    [ADD]add categories tab

---
+++ src/app/[locale]/admin/categories/page.tsx
@@ -0,0 +1,38 @@
+import { Locale } from "@/i18n.config";
+import getTrans from "@/lib/translation";
+import { getCategories } from "@/server/db/categories";
+
+async function CategoriesPage({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
+  const locale = (await params).locale;
+  const categories = await getCategories();
+  const translations = await getTrans(locale);
+
+  return (
+    <main>
+      <section className="section-gap">
+        <div className="container">
+          <div className="sm:max-w-[625px] mx-auto space-y-6">
+            {/* <Form translations={translations} /> */}
+            {categories.length > 0 ? (
+              <ul className="flex flex-col gap-4">
+                {/* {categories.map(() => (
+                  // <CategoryItem key={category.id} category={category} />
+                ))} */}
+              </ul>
+            ) : (
+              <p className="text-accent text-center py-10">
+                {translations.noCategoriesFound}
+              </p>
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default CategoriesPage;
